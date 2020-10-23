1ada6943ca6b9dac02ed02af17acb5e03900fd45
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-10-23 03:46:02 +0200

    Application class

--- config/config.php
+++ config/config.php
@@ -1,3 +1,5 @@
 <?php
 
+define('DEBUG', true);
+
 define('DEFAULT_CONTROLLER', 'Home'); // default controller if is not specified in the URL
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -16,4 +16,3 @@
 
 //    Route the request
     Router::route($url);
-
