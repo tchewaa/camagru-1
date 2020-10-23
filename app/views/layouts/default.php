87c7896fb2c6251b89288e65ea8f3646fe26b697
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-10-23 18:36:45 +0200

    fixed the bug that was happening when I try to load the default, I forgot to put a '.' before php on the View class

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Camagru 2.0</title>
     <link rel="stylesheet" href="<?=PROOT?>css/bootstrap.min.css" media="screen" title="no title" charset="UTF-8">
     <link rel="stylesheet" href="<?=PROOT?>css/custom.css" media="screen" title="no title" charset="UTF-8">
     <script src="<?=PROOT?>js/jQuery-2.2.4.min.js"></script>
@@ -17,6 +17,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <?=$this->content('body'); ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
--- core/View.php
+++ core/View.php
@@ -12,7 +12,7 @@ class View {
         $viewString = implode(DS, $viewArray);
         if (file_exists(ROOT . DS . 'app' . DS . 'views' . DS . $viewString . '.php')) {
             include(ROOT . DS . 'app' . DS . 'views' . DS . $viewString . '.php');
-            include(ROOT . DS . 'app' . DS . 'views' . DS . 'layouts' . DS . $this->_layout . 'php');
+            include(ROOT . DS . 'app' . DS . 'views' . DS . 'layouts' . DS . $this->_layout . '.php');
         } else {
             die('The view \"' . $viewName . '\" does not exist');
         }
