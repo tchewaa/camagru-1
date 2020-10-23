11ba2b1ca81201f1f93b719aca890e587baad62d
martian1431 <pmalope@student.wethinkcode.co.za>
2020-10-23 03:13:53 +0200

    working on the router

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php
+
+define('DEFAULT_CONTROLLER', 'Home'); // default controller if is not specified in the URL
--- core/Router.php
+++ core/Router.php
@@ -4,6 +4,25 @@
 class Router {
 
     public static function route($url) {
-        dnd($url);
+        //controller
+        $controller = (isset($url[0]) && $url[0] != '') ? ucwords($url[0]): DEFAULT_CONTROLLER;
+        $controller_name = $controller;
+        array_shift($url);
+
+        //action
+        $action = (isset($url[0]) && $url[0] != '') ? $url[0] . 'Action': 'indexAction';
+        $action_name = $controller;
+        array_shift($url);
+
+        //params
+        $queryParams = $url;
+
+        $dispatch = new $controller($controller_name, $action);
+
+        if (method_exists($controller, $action)) {
+            call_user_func([$dispatch, $action], $queryParams);
+        } else {
+            die('That method does not exist in the controller \"' . $controller_name . '\"');
+        }
     }
 }
