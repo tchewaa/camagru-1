c267e53a2d366455e33a3841a7d414a5e428929b
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-10-24 04:30:34 +0200

    refactoring

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,3 @@
-<?php //$this->setSiteTitle('Home'); ?>
-<?php $this->start('head'); ?>
-<meta content="Testing">
-<?php $this->end(); ?>
 <?php $this->start('body'); ?>
-<h1>Welcome to camagru 2.0</h1>
+<h1 class="test">Welcome to camagru 2.0</h1>
 <?php $this->end(); ?>
--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -5,22 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Camagru 2.0</title>
+    <title><?=$this->siteTitle(); ?></title>
     <link rel="stylesheet" href="<?=PROOT?>css/bootstrap.min.css" media="screen" title="no title" charset="UTF-8">
     <link rel="stylesheet" href="<?=PROOT?>css/custom.css" media="screen" title="no title" charset="UTF-8">
     <script src="<?=PROOT?>js/jQuery-2.2.4.min.js"></script>
     <script src="<?=PROOT?>js/bootstrap.min.js"></script>
 
     <?= $this->content('head'); ?>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <?=$this->content('body'); ?>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+    <?= $this->content('body'); ?>
 </body>
 </html>
