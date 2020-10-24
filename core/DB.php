9c34e763d417d291a8884cb741774fe5069c4a25
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-10-24 18:29:28 +0200

    new file - database wrapper, added the following methods: query, insert, update, delete, error. The wrapper is working as expected

---
+++ core/DB.php
@@ -0,0 +1,96 @@
+<?php
+
+
+class DB {
+    private static $_instance = null;
+    private $_pdo, $_query, $_error = false, $_result, $_count = 0, $_lastInsertID = null;
+
+    private function __construct() {
+        try {
+            $this->_pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
+            //TODO automate creating database and tables
+       } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (!isset(self::$_instance)) {
+            self::$_instance = new DB();
+        }
+        return self::$_instance;
+    }
+
+    public function query($sql, $params = []) {
+        $this->_error = false;
+        if ($this->_query = $this->_pdo->prepare($sql)) {
+            $index = 1;
+            if (count($params)) {
+                foreach ($params as $param) {
+                   $this->_query->bindValue($index, $param);
+                   $index++;
+                }
+            }
+
+            if ($this->_query->execute()) {
+                $this->_result = $this->_query->fetchAll(PDO::FETCH_OBJ);
+                $this->_count = $this->_query->rowCount();
+                $this->_lastInsertID = $this->_pdo->lastInsertId();
+            } else {
+                $this->_error = true;
+            }
+        }
+        return $this;
+    }
+
+    public function insert($table, $fields = []) {
+        $fieldString = '';
+        $valueString = '';
+        $values = [];
+
+        foreach ($fields as $field => $value) {
+            $fieldString .= '`' . $field . '`,';
+            $valueString .= '?,';
+            $values[] = $value;
+        }
+        $fieldString = rtrim($fieldString, ',');
+        $valueString = rtrim($valueString, ',');
+        $sql = "INSERT INTO {$table} ({$fieldString}) VALUES ({$valueString})";
+        if (!$this->query($sql, $values)->error()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function update($table, $id, $fields = []) {
+        $fieldString = '';
+        $values = [];
+
+        foreach ($fields as $field => $value) {
+            $fieldString .= ' ' . $field . ' = ?,';
+            $values[] = $value;
+        }
+
+        $fieldString = trim($fieldString);
+        $fieldString = rtrim($fieldString, ',');
+        $sql = "UPDATE {$table} SET {$fieldString} WHERE id = {$id}";
+
+        if (!$this->query($sql, $values)->error()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function delete($table, $id) {
+        $sql = "DELETE FROM {$table} WHERE id = {$id}";
+
+        if (!$this->query($sql)->error()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function error() {
+        return $this->_error;
+    }
+}
