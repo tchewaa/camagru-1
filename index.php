e99fec54077eefe8a1358b0c2ddf657a1e718d99
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-10-24 18:26:05 +0200

    importing database config file

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
     //    load configuration and helper functions
     require_once(ROOT . DS . 'config' . DS . 'config.php');
+    require_once(ROOT . DS . 'config' . DS . 'database.php');
     require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
 
     //    Autoload classes
