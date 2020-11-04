261da4e75562434f6a6bd99e706f23051c5162ce
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-04 11:03:25 +0200

    displaying validation messages

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -8,7 +8,7 @@ use Core\FormHelper;
     <h3 class="text-center">Log In</h3>
     <form class="form" action="" method="post">
         <?= FormHelper::csrfInput() ?>
-        <?= FormHelper::displayValidationMessage($this->validationMessages) ?>
+        <?= FormHelper::displayValidationMessage((isset($this->validationMessages) ? $this->validationMessages : '')) ?>
         <?= FormHelper::inputBlock('text','Username','username',$this->auth->username,['class'=>'form-control'],['class'=>'form-group']) ?>
         <?= FormHelper::inputBlock('password','Password','password',$this->auth->password,['class'=>'form-control'],['class'=>'form-group']) ?>
         <?= FormHelper::checkboxBlock('Remember Me','remember_me',$this->auth->getRememberMeChecked(),[],['class'=>'form-group']) ?>
