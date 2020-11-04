d239e760b9045967340aceb8d60e72308707dc8a
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-04 11:04:56 +0200

    refactoring

--- app/views/register/register.php
+++ app/views/register/register.php
@@ -9,11 +9,13 @@ use Core\FormHelper;
     <form class="form" action="<?=PROOT?>register/register" method="post">
         <?= FormHelper::csrfInput() ?>
         <?= FormHelper::displayValidationMessage((isset($this->validationMessages) ? $this->validationMessages : '')) ?>
-        <?= FormHelper::inputBlock('text','First Name','first_name',(isset($this->user) ? $this->user->first_name : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
-        <?= FormHelper::inputBlock('text','Last Name','last_name',(isset($this->user) ? $this->user->last_name : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
+        <?= FormHelper::inputBlock(
+                'text','Username',
+                'username',
+                (isset($this->user) ? $this->user->username : ''),
+                ['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
         <?= FormHelper::inputBlock('email','Email','email',(isset($this->user) ? $this->user->email : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
-        <?= FormHelper::inputBlock('text','Username','username',(isset($this->user) ? $this->user->username : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
-        <?= FormHelper::inputBlock('password','Password','password',(isset($this->user) ? $this->user->last_name : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
+        <?= FormHelper::inputBlock('password','Password','password',(isset($this->user) ? $this->user->password : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
         <?= FormHelper::inputBlock('password','Confirm Password','confirm',(isset($this->user) ? $this->user->getConfirm() : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
         <?= FormHelper::submitBlock('Register',['class'=>'btn btn-primary btn-large'],['class'=>'form-group']) ?>
         <div class="text-right">
