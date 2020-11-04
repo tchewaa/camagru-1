23ccbc1812caa9d905bde3f9657e36522295f170
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-04 19:13:01 +0200

    added default page for register view

---
+++ app/views/register/index.php
@@ -0,0 +1,26 @@
+<?php
+use Core\FormHelper;
+?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+<div class="col-md-4 col-md-offset-4 jumbotron">
+    <h3 class="text-center">Register Here!</h3><hr>
+    <form class="form" action="<?=PROOT?>register/registerUser" method="post">
+        <?= FormHelper::csrfInput() ?>
+        <?= FormHelper::displayValidationMessage((isset($this->validationMessages) ? $this->validationMessages : '')) ?>
+        <?= FormHelper::inputBlock(
+                'text','Username',
+                'username',
+                (isset($this->user) ? $this->user->username : ''),
+                ['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
+        <?= FormHelper::inputBlock('email','Email','email',(isset($this->user) ? $this->user->email : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
+        <?= FormHelper::inputBlock('password','Password','password',(isset($this->user) ? $this->user->password : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
+        <?= FormHelper::inputBlock('password','Confirm Password','confirm',(isset($this->user) ? $this->user->getConfirm() : ''),['class'=>'form-control input-sm'],['class'=>'form-group']) ?>
+        <?= FormHelper::submitBlock('Register',['class'=>'btn btn-primary btn-large'],['class'=>'form-group']) ?>
+        <div class="text-right">
+            <a href="<?=PROOT?>register/resendToken">Resend Verification</a>
+        </div>
+    </form>
+</div>
+<?php $this->end(); ?>
