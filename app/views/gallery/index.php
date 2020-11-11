f0771ea370bfba4ba8954789e7bfb7e060bea2fd
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-11 22:16:00 +0200

    gallery layout

--- app/views/gallery/index.php
+++ app/views/gallery/index.php
@@ -1,28 +1,35 @@
 <?php $this->setSiteTitle('upload or take a pic'); ?>
 <?php $this->start('body'); ?>
 <div class="primary">
-    <div class="side-bar sidebar jumbotron">
-        <h4>Select a frame for your picture</h4>
-        <nav class="sidebar-nav">
-            <ul class="nav">
-                <?php
-                //TODO refactor: move to Helpers class - getStickers()
-                $fileDir = ROOT . DS . 'app' . DS . 'assets' . DS . 'stickers';
-                if (is_dir($fileDir)) {
-                    $files = scandir($fileDir);
-                    $itr = 2;
-                    while ($itr < count($files)) {
-                        echo '<li class="nav-item">';
-                        echo "<img src=\"" . PROOT . 'app' . DS . 'assets' . DS . "stickers" . DS . "{$files[$itr]}\" class=\"frame sticker\" alt=\"frame\">";
-                        echo '</li>';
-                        $itr++;
-                    }
+    <div class="side-bar jumbotron">
+        <h4>Select a Sticker</h4>
+        <ul class="nav">
+            <?php
+            //TODO refactor: move to Helpers class - getStickers()
+            $fileDir = ROOT . DS . 'app' . DS . 'assets' . DS . 'stickers';
+            if (is_dir($fileDir)) {
+                $files = scandir($fileDir);
+                $itr = 2;
+                while ($itr < count($files)) {
+                    echo '<li class="nav-item">';
+                    echo "<img src=\"" . PROOT . 'app' . DS . 'assets' . DS . "stickers" . DS . "{$files[$itr]}\" class=\"frame sticker\" alt=\"frame\">";
+                    echo '</li>';
+                    $itr++;
                 }
-                ?>
-            </ul>
-        </nav>
+            }
+            ?>
+        </ul>
     </div>
     <div class="main-section" style="flex: 2;" class="container">
+        <canvas id="canvas" style="display: none;"></canvas>
+        <br>
+        <center><canvas id="imageCanvas" style="width: 100%; height: 100%; max-height: 480px; max-width: 640px;"></canvas></center>
+        <center>
+            <div class="camera">
+                <video id="video" style="width: 100%; height: 100%; max-height: 480px; max-width: 640px;">Video stream not available</video> <br />
+                <button id="startbutton" class="btn btn-primary btn-large" style="display: inline-block;">Take photo</button>
+            </div>
+        </center>
         <div class="col-md-6 col-md-offset-3 jumbotron submit-form">
             <form action="<?=PROOT?>gallery/upload" method="post" enctype="multipart/form-data" name="get_image">
                 <label>Image File:</label><br/>
@@ -35,20 +42,11 @@
                 </div>
             </form>
         </div>
-        <canvas id="canvas" style="display: none;"></canvas>
-        <br>
-        <center><canvas id="imageCanvas" style="width: 100%; height: 100%; max-height: 480px; max-width: 640px;"></canvas></center>
-        <center>
-        <div class="camera">
-            <video id="video" style="width: 100%; height: 100%; max-height: 480px; max-width: 640px;">Video stream not available</video> <br />
-            <button id="startbutton" class="btn btn-primary btn-large" style="display: inline-block;">Take photo</button>
-        </div>
-        </center>
         <script src="#"></script>
         <script src="#"></script>
     </div>
-    <div class="end-section" style="flex: 3; max-width:300px;">
-
+    <div class="side-bar jumbotron">
+        <h4>Your Images:</h4>
     </div>
 </div>
 <?php $this->end(); ?>
