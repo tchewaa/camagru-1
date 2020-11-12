b2a66a0b4abda7639576b9790fb280896a52f189
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-12 21:40:35 +0200

    aligned stickers and buttons

--- app/views/gallery/index.php
+++ app/views/gallery/index.php
@@ -6,13 +6,45 @@
             <div class="row">
                 <div class="col-md-6">
                     <h3>Editor</h3>
-                    <form>
-                        <input id="file-upload" class="p-0" required name="photo" type="file" accept="image/jpeg, image/png, application/pdf" />
-                        <!--                <button> Take Photo</button>-->
-                    </form>
+                    <video id="video">Stream not available</video>
+                    <button class="btn btn-dark" id="photo-button">
+                        Take Photo
+                    </button>
+                    <input name="image" id="image-upload" type="file"/>
+<!--                    <select class="btn btn-dark" id="photo-filter" >-->
+<!--                        <option value="none">Normal</option>-->
+<!--                        <option value="grayscale(100%)">Grayscale</option>-->
+<!--                        <option value="sepia(100%)">Sepia</option>-->
+<!--                        <option value="invert(100%)">Invert</option>-->
+<!--                        <option value="hue-rotate(90deg)">Hue</option>-->
+<!--                        <option value="blur(10px)">Blur</option>-->
+<!--                        <option value="contrast(200%)">Contrast</option>-->
+<!--                    </select>-->
+                    <div id="stickers">
+                        <input type="checkbox" name="sticker-menu2" id="sticker1" value="">
+                        <label for="sticker1">
+                            <img src="<?=PROOT?>app/assets/stickers/580b57fcd9996e24bc43c319.png" width="100px" height="100px"  alt="sticker 1">
+                        </label>
+                        <input type="checkbox" name="sticker-menu2" id="sticker1" value="">
+                        <label for="sticker2">
+                            <img src="<?=PROOT?>app/assets/stickers/580b57fcd9996e24bc43c319.png" width="100px" height="100px"  alt="sticker 1">
+                        </label>
+                        <input type="checkbox" name="sticker-menu2" id="sticker1" value="">
+                        <label for="sticker1">
+                            <img src="<?=PROOT?>app/assets/stickers/580b57fcd9996e24bc43c319.png" width="100px" height="100px"  alt="sticker 1">
+                        </label>
+                        <input type="checkbox" name="sticker-menu2" id="sticker1" value="">
+                        <label for="sticker2">
+                            <img src="<?=PROOT?>app/assets/stickers/580b57fcd9996e24bc43c319.png" width="100px" height="100px"  alt="sticker 1">
+                        </label>
+                    </div>
                 </div>
                 <div class="col-md-6">
                     <h3>Captured Image</h3>
+                    <canvas id="canvas"></canvas>
+                    <button class="btn btn-dark" id="clear-button">Clear</button>
+                    <!--TODO do I need this-->
+                    <div id="photos"></div>
                 </div>
             </div>
         </div>
