4aefedd59d63ea851249ce04e49e01b3a5e5556d
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-12 03:29:29 +0200

    gallery layout

--- app/views/gallery/index.php
+++ app/views/gallery/index.php
@@ -1,47 +1,31 @@
 <?php $this->setSiteTitle('upload or take a pic'); ?>
 <?php $this->start('body'); ?>
-<div class="primary">
-    <div class="side-bar jumbotron">
-        <h4>Select a Sticker</h4>
-        <ul class="nav">
-            <?php
-            //TODO refactor: move to Helpers class - getStickers()
-            $fileDir = ROOT . DS . 'app' . DS . 'assets' . DS . 'stickers';
-            if (is_dir($fileDir)) {
-                $files = scandir($fileDir);
-                $itr = 2;
-                while ($itr < count($files)) {
-                    echo '<li class="nav-item">';
-                    echo "<img src=\"" . PROOT . 'app' . DS . 'assets' . DS . "stickers" . DS . "{$files[$itr]}\" class=\"frame sticker\" alt=\"frame\">";
-                    echo '</li>';
-                    $itr++;
-                }
-            }
-            ?>
-        </ul>
-    </div>
-    <div class="main-section">
-        <div class="top-container">
-            <video id="video">Stream not available</video>
-            <button class="btn btn-dark" id="photo-button">
-                Take Photo
-            </button>
-            <select id="photo-filter">
-                <option value="none">Normal</option>
-                <option value="grayscale(100%)">Grayscale</option>
-                <option value="sepia(100%)">Sepia</option>
-                <option value="invert(100%)">Invert</option>
-                <option value="hue-rotate(90deg)">Hue</option>
-                <option value="blur(10px)">Blur</option>
-                <option value="contrast(200%)">Contrast</option>
-            </select>
-            <button class="btn btn-dark" id="clear-button">Clear</button>
-            <canvas id="canvas"></canvas>
+    <div class="row">
+        <div class="col-md-8 jumbotron">
+            <h3>Upload or Take a Picture</h3>
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Video</h3>
+                </div>
+                <div class="col-md-6">
+                    <h3>Captured Image</h3>
+                </div>
+            </div>
         </div>
-        <div class="bottom-container">
-            <div id="photos"></div>
+        <div class="col-md-4 sidebar">
+            <h3>Your images</h3>
+            <div class="row">
+                <div class="col-sm-6 thumbnail">
+                    <img src="<?=PROOT?>app/assets/dummy/sanfran.jpg" class="img-rounded" alt="sanfran">
+                </div>
+                <div class="col-sm-6 thumbnail">
+                    <img src="<?=PROOT?>app/assets/dummy/sanfran.jpg" class="img-rounded" alt="sanfran">
+                </div>
+                <div class="col-sm-6 thumbnail">
+                    <img src="<?=PROOT?>app/assets/dummy/sanfran.jpg" class="img-rounded" alt="sanfran">
+                </div>
+            </div>
         </div>
     </div>
-</div>
 <script type="text/javascript" src="<?=PROOT?>js/webcam.js"></script>
 <?php $this->end(); ?>
