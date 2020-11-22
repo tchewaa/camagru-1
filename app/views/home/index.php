f8056eafc6f8847a079860849dc508d9cf4e08df
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-22 07:21:10 +0200

    testing pagination

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,5 +5,12 @@ use Core\Helper;
 <h1 class="test">Welcome to Camagru</h1>
 <div class="gallery">
     <?=Helper::displayGalleryImages($this->images) ?>
+    <ul class="pagination">
+        <?php
+            echo '<li><a href="http://localhost/camagru/home/index/1" class="pages" id="beginPages">Begin</a></li>';
+//            echo '<li><a href="http://localhost/camagru/home/index/1" class="pages" id="beginPages">Test 1</a></li>';
+            echo '<li><a href="http://localhost/camagru/home/index/1" class="pages" id="endPages">End</a></li>';
+        ?>
+    </ul>
 </div>
 <?php $this->end(); ?>
