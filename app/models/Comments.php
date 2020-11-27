ab9abc4d4a271fa8de6761c096150371b320895d
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-27 03:03:04 +0200

    new file

---
+++ app/models/Comments.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Core\Helper;
+use Core\Model;
+use Core\Validators\MaxValidator;
+use Core\Validators\MinValidator;
+
+class Comments extends Model {
+    public $id;
+    public $user_id;
+    public $image_id;
+    public $content;
+
+    public function __construct(){
+        $table = 'comments';
+        parent::__construct($table);
+    }
+
+    public function comment($comment, $imageId) {
+        $currentUser = Users::currentUser();
+        $this->user_id = $currentUser->id;
+        $this->image_id = $imageId;
+        $this->content = $comment;
+        if ($this->save()) {
+            Helper::dnd($this->getErrorMessages());
+        }
+    }
+
+    //TODO add validations
+    public function validator() {
+        $this->runValidation(new MinValidator($this, ['field' => 'content', 'rule' => 1, 'msg' => 'Comment must at least have 1 character']));
+        $this->runValidation(new MaxValidator($this, ['field' => 'content', 'rule' => 255, 'msg' => 'Comment cannot be more that 255 characters']));
+    }
+
+}
