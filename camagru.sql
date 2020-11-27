4832e9b5097d9b9a74ce5f55367c9c0856885ed6
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-27 03:03:23 +0200

    added date column on comments

--- camagru.sql
+++ camagru.sql
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `user_id` int(11) NOT NULL,
     `image_id` int(11) NOT NULL,
-    `content` text NOT NULL,
+    `content` varchar(255) NOT NULL,
     `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES users(id),
