3721806be9e83c999a34b295396a4ea710fee6e1
martian1431 <pmalope@student.wethinkcode.co.za>
2020-11-29 16:22:07 +0200

    spinner working on comment button

--- js/main.js
+++ js/main.js
@@ -15,22 +15,21 @@ likeButton.addEventListener('DOMContentLoaded', function (e) {
 });
 
 likeButton.addEventListener('click', function(e) {
-    const formData = new FormData();
-    const imageId = likeButton.value;
-    const likeStatus = likeButton.innerText;
-    const url = (host.indexOf("Windows")) ? "http://localhost/camagru/home/like" : "http://localhost:8080/camagru/image/like";
-    formData.append('image-id', imageId);
-    formData.append('like-status', likeStatus);
     loadSpinner.classList.add('loading');
 
     try {
-        setTimeout( async function () {
+        const formData = new FormData();
+        const imageId = likeButton.value;
+        const likeStatus = likeButton.innerText;
+        const url = (host.indexOf("Windows")) ? "http://localhost/camagru/home/like" : "http://localhost:8080/camagru/image/like";
+        formData.append('image-id', imageId);
+        formData.append('like-status', likeStatus);
 
-            await fetch(url, {
+        setTimeout( async function () {
+            const response = await fetch(url, {
                 method: 'POST',
                 body: formData
             });
-
             window.location.reload();
         }, 2000)
 
@@ -45,27 +44,29 @@ const commentButton = document.getElementById('comment-button');
 const commentText = document.getElementById('comment-text');
 
 commentButton.addEventListener('click', function (e) {
-    if (commentText.value !== '') {
-        const formData = new FormData();
-        const comment = commentText.value;
-        const imageId = commentButton.value;
-        formData.append('image-id', imageId);
-        formData.append('comment', comment);
-        const url = (host.indexOf("Windows")) ? "http://localhost/camagru/home/comment" : "http://localhost:8080/camagru/image/comment";
-        fetch(url, {
-            method: 'POST',
-            body: formData
-        }).then(
-            response => response.text()
-        ).then(
-            data => {
-                // console.log(data);
-                window.location.reload();
+    loadSpinner.classList.add('loading');
 
-            }
-        )
-        e.preventDefault();
-    } else {
-        console.log("You cannot post empty comment...");
+    try {
+        if (commentText.value !== '') {
+            const formData = new FormData();
+            const comment = commentText.value;
+            const imageId = commentButton.value;
+            formData.append('image-id', imageId);
+            formData.append('comment', comment);
+            const url = (host.indexOf("Windows")) ? "http://localhost/camagru/home/comment" : "http://localhost:8080/camagru/image/comment";
+
+            setTimeout( async function () {
+                const response = await fetch(url, {
+                    method: 'POST',
+                    body: formData
+                });
+                window.location.reload();
+            }, 2000)
+            e.preventDefault();
+        } else {
+            alert("You cannot post empty comment...");
+        }
+    } catch (e) {
+        alert("Something went wrong...");
     }
 });
